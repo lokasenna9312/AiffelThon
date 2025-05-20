1b7c4405120b061d387932fdb68a44efb4379786
Lokasenna <solentis1384@gmail.com>
2025-05-20 16:10:19 +0900

    AppBar의 버튼이 로긴 상태에서만 표시되도록 수정

--- lib/appbar.dart
+++ lib/appbar.dart
@@ -14,37 +14,48 @@ class CSAppBar extends StatelessWidget implements PreferredSizeWidget {
   // 로그인, 로그아웃 기능은 register.dart 파일에서 다룹니다.
   @override
   Widget build(BuildContext context) {
+
+    final userDataProvider = Provider.of<UserDataProvider>(context);
+    final bool isLoggedIn = userDataProvider.isLoggedIn;
+
     return AppBar(
       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       title: Text(title),
       actions: <Widget>[
-        ElevatedButton(
-          child: Text('로그아웃'),
-          onPressed: () {
-            Provider.of<UserDataProvider>(context, listen: false).logoutUser();
-            // 로그아웃 후 필요한 화면 전환 (예: 로그인 페이지로 이동)은 해당 위젯 또는 상위에서 처리
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(content: Text('로그아웃 되었습니다.')),
-            );
-            Navigator.pushReplacement(
-              context,
-              MaterialPageRoute(
-                builder: (context) => CSHomePage(CSTitle: title),
+        if (isLoggedIn)
+          Row(
+            children: [
+              ElevatedButton(
+                child: Text('로그아웃'),
+                onPressed: () {
+                  Provider
+                      .of<UserDataProvider>(context, listen: false)
+                      .logoutUser();
+                  // 로그아웃 후 필요한 화면 전환 (예: 로그인 페이지로 이동)은 해당 위젯 또는 상위에서 처리
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('로그아웃 되었습니다.')),
+                  );
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CSHomePage(CSTitle: title),
+                    ),
+                  );
+                }
               ),
-            );
-          }
-        ),
-        ElevatedButton(
-            child: Text('탈퇴'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => WithdrawPage(CSTitle: title),
-                ),
-              );
-            }
-        ),
+              ElevatedButton(
+                child: Text('탈퇴'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => WithdrawPage(CSTitle: title),
+                    ),
+                  );
+                }
+              ),
+            ]
+          )
       ],
     );
   }
